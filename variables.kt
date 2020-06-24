a2e3192b15aff5bc01d99b1086119e9e2ee2b798
laymoncoder <67359823+LaymonCoder@users.noreply.github.com>
2020-06-24 18:40:35 +0530

    Variables.kt file added

---
+++ variables.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var myFirstVariable = "Hello Coder"
+    var myFirstNumber:Int = 10
+    var myFirstDecimal = 3.14
+    var myFirstCharacter = 'A'
+    var myFirstString = "Kotlin"
+    var myFirstBoolean = true
+    println(myFirstVariable)
+    println(myFirstNumber)
+    println(myFirstDecimal)
+    println(myFirstCharacter)
+    println(myFirstString)
+    println(myFirstBoolean)
+}
