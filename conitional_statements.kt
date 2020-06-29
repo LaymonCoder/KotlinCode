297d3ef7ebb8c41412c770124d62240139e28d11
laymoncoder <67359823+LaymonCoder@users.noreply.github.com>
2020-06-29 19:09:15 +0530

    Added code example  for conditional_statements

---
+++ conitional_statements.kt
@@ -0,0 +1,15 @@
+fun main() {
+   var age = 19
+   if(age >= 18){
+    println("You are eligible to vote")
+   }
+   else{
+    println("You are not eligible to vote")
+   }
+   var marks = 54
+   if(marks >= 90) println("A+")
+   else if(marks >= 80) println("A")
+   else if(marks >= 70) println("B")
+   else if(marks >= 60) println("C") 
+   else println("D")
+}
